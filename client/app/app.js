3cdb6f92e0d279433855ae4f984f6a69528b978f
Bruno Zatta <bzatta@gmail.com>
2016-04-17 10:55:03 -0400

    add new module dependency to app, add config for marked provider and provide to setup wysiwyg

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,44 @@
-var app = angular.module('notify', ['ui.router', 'ngMessages']);
+var app = angular.module('notify', ['ui.router', 'ngMessages', 'hc.marked', 'textAngular']);
 
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', 'markedProvider', '$provide', function($stateProvider, $urlRouterProvider, markedProvider, $provide) {
 
+  markedProvider.setOptions({
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false,
+    highglight: function(code, lang) {
+      if (lang) {
+        return hljs.highlight(lang, code, true).value;
+      } else {
+        return hljs.highlightAuto(code).value;
+      }
+    }
+  });
+
+
+ $provide.decorator('taOptions', ['$delegate', function(taOptions){
+    // $delegate is the taOptions we are decorating
+    // here we override the default toolbars and classes specified in taOptions.
+    taOptions.forceTextAngularSanitize = true; // set false to allow the textAngular-sanitize provider to be replaced
+    taOptions.keyMappings = []; // allow customizable keyMappings for specialized key boards or languages
+    taOptions.toolbar = [['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote', 'bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear', 'justifyLeft','justifyCenter','justifyRight', 'justifyFull', 'html', 'insertImage', 'insertLink', 'wordcount', 'charcount'
+    ]];
+    taOptions.classes = {
+        focussed: 'focussed',
+        toolbar: 'btn-toolbar',
+        toolbarGroup: 'btn-group',
+        toolbarButton: 'btn btn-default',
+        toolbarButtonActive: 'active',
+        disabled: 'disabled',
+        textEditor: 'form-control',
+        htmlEditor: 'form-control'
+    };
+    return taOptions; // whatever you return will be the taOptions
+  }]);
 
   $stateProvider
     .state('app', {
