ade4b8a428d3937baeb2ac0305bc50fdf598914d
Bruno Zatta <bzatta@gmail.com>
2016-04-18 11:41:26 -0400

    pre deployment updates

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 // var app = angular.module('notify', ['ui.router', 'ngMessages', 'hc.marked', 'textAngular']);
 
-var app = angular.module('notify', ['ui.router', 'ngMessages', 'hc.marked']);
+var app = angular.module('notify', ['ui.router', 'ngMessages', 'hc.marked', 'textAngular']);
 
 app.config(['$stateProvider', '$urlRouterProvider', '$provide', function($stateProvider, $urlRouterProvider, $provide) {
 
