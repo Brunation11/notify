7c0358ba2f1ea06f598513bf2ee5ab3edfcd5943
Bruno Zatta <bzatta@gmail.com>
2016-04-18 10:52:13 -0400

    pre deployment updates

--- client/navBar/controller.js
+++ client/navBar/controller.js
@@ -3,9 +3,6 @@ var app = angular.module('notify');
 app.controller('navController', ['$scope', 'authService', '$state', function($scope, authService, $state) {
 
   $scope.loggedIn = authService.loggedIn;
-  $scope.logout = function() {
-    authService.logout;
-    $state.go('app');
-  }
+  $scope.logout = authService.logout;
 
 }]);
